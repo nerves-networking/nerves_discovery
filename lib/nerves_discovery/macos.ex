nerves-networking/nerves_discovery

commit 980933e94c93c721510f57b4993de8293896fd15

Fix timeout to avoid infinite wait on macOS
Apparently 1 second timeouts worked pretty well. <1 second timeouts
would be infinite, though. This was caught when unit testing short
timeouts.

lib/nerves_discovery/macos.ex
@@ -10,7 +10,7 @@ defmodule NervesDiscovery.MacOS do
   """
   @spec discover_service(String.t(), non_neg_integer()) :: [map()]
   def discover_service(service, timeout) do
-    timeout_secs = min(div(timeout, 1000), 1)
+    timeout_secs = max(div(timeout, 1000), 1)
 
     {output, _} =
       System.cmd("timeout", [to_string(timeout_secs), "dns-sd", "-B", service],

commit 0c4364e0d7127c0e4a13b72e07cd2ec0e9d23f83

Minimally fix detection timeout on macOS
This fixes a regression introduced by
980933e94c93c721510f57b4993de8293896fd15 that causes the detector to
time out pretty much all the time.

lib/nerves_discovery/macos.ex
@@ -10,7 +10,10 @@ defmodule NervesDiscovery.MacOS do
   """
   @spec discover_service(String.t(), non_neg_integer()) :: [map()]
   def discover_service(service, timeout) do
-    timeout_secs = max(div(timeout, 1000), 1)
+    # Use half the time to scan for devices, but force it to be between
+    # 100ms and 1s. The max time to wait is arbitrary. This should be
+    # rewritten to stream results.
+    timeout_secs = (timeout / 2 / 1000) |> max(0.1) |> min(1)
 
     {output, _} =
       System.cmd("timeout", [to_string(timeout_secs), "dns-sd", "-B", service],
@@ -27,7 +30,7 @@ defmodule NervesDiscovery.MacOS do
     end)
     |> Task.async_stream(&resolve_device(&1, service),
       max_concurrency: 10,
-      timeout: div(timeout, 1000) * 1000,
+      timeout: 1500,
       on_timeout: :kill_task
     )
     |> Enum.flat_map(fn